d6f28746311a28af1d253d782916944f97977d43
Robert C Edwards <Github@coactlab.com>
2014-09-20 20:58:10 -0700

    switching to f

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 navigator.geolocation.getCurrentPosition(function(pos) {
   var coords = pos.coords;
   var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
-      'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+      'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=imperial;
   ajax({ url: weatherUrl, type: 'json' }, function(data) {
     simply.text({ title: data.name, subtitle: data.main.temp });
   });
