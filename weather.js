eb2f354a0acc0d34858c0384b22bc29d370a4eeb
Robert C Edwards <Github@coactlab.com>
2014-09-20 20:54:09 -0700

    asd

--- weather.js
+++ weather.js
@@ -1,8 +1,19 @@
-var weatherUrl = 'http://geonet.org.nz/quakes/services/quake/2014p088748.json';
-
-ajax({ url: weatherUrl, type: 'json' }, function(data) {
-    simply.text({
-    title: data.features[0].properties.magnitude,
-    subtitle: data.features[0].properties.depth,
-    body: data.features[0].properties.intensity
+var parada = "0001"
+ajax({ url: 'http://www.tmb.cat/piu/ca_ES/piuimodesolucio.jsp?parada='
++ parada }, function(data){
+  var info = data.match(/<td align="center">(.*?)<\/td>/g);
+  simply.title('Parada ' + parada);
+  var output = "";
+  for (var i = 2; i<info.length; i++)
+  {
+    output = output + " " + info[i];
+    if (i%2!=0) output = output + "\n"
+  }
+  simply.body(clean(output));
 });
+
+function clean(text)
+{
+  return text.replace(/<td align=\"center\">/g,'').replace(/<\/td>/g,
+'').replace(/<b>/g,'').replace(/<\/b>/g,'');
+}
