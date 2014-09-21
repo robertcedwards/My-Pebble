7241927115bf5c96d735751ed9e7caaba6f85bba
Robert C Edwards <Github@coactlab.com>
2014-09-20 20:56:29 -0700

    adding geolocation weather example

--- weather.js
+++ weather.js
@@ -1,19 +1,8 @@
-var parada = "0001"
-ajax({ url: 'http://www.tmb.cat/piu/ca_ES/piuimodesolucio.jsp?parada='
-+ parada }, function(data){
-  var info = data.match(/<td align="center">(.*?)<\/td>/g);
-  simply.title('Parada ' + parada);
-  var output = "";
-  for (var i = 2; i<info.length; i++)
-  {
-    output = output + " " + info[i];
-    if (i%2!=0) output = output + "\n"
-  }
-  simply.body(clean(output));
+navigator.geolocation.getCurrentPosition(function(pos) {
+  var coords = pos.coords;
+  var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
+      'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+  ajax({ url: weatherUrl, type: 'json' }, function(data) {
+    simply.text({ title: data.name, subtitle: data.main.temp });
+  });
 });
-
-function clean(text)
-{
-  return text.replace(/<td align=\"center\">/g,'').replace(/<\/td>/g,
-'').replace(/<b>/g,'').replace(/<\/b>/g,'');
-}
